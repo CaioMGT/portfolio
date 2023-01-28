b5e54638c4e4b721bfb9a188f1f3b02c7ba33f88
CaioMGT <florianita100.mendes@gmail.com>
2023-01-28 17:06:36 -0300

    Separate theme switcher from theme manager

---
+++ thememgr.js
@@ -0,0 +1,19 @@
+const Enum = {
+    LIGHT: 1,
+    DARK : 2
+}
+let theme = localStorage.getItem("Theme")
+changetheme(theme)
+function changetheme(theme, set) {
+    let html = document.getElementById("html")
+    if (set) {
+        localStorage.setItem("Theme", theme)
+    }
+    if (theme == Enum.LIGHT) {
+        html.className = ""
+        theme = Enum.LIGHT
+    } else {
+        html.className = "dark"
+        theme = Enum.DARK
+    }
+}
