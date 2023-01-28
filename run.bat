92b78370d08140cdeb5bbfea4cbd849204ae0ff8
CaioMGT <florianita100.mendes@gmail.com>
2023-01-28 14:40:24 -0300

    Separate 404 page and index page's css files

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-npx tailwindcss -i ./styles.css -o ./dist/output.css --watch
+START npx tailwindcss -i ./styles.css -o ./dist/output.css --watch
+START npx tailwindcss -i ./nopage.css -o ./dist/nopage.css --watch
