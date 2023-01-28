4b38f567d49ccc7fccee563386f280fb92102e33
CaioMGT <florianita100.mendes@gmail.com>
2023-01-28 14:42:56 -0300

    Fix 404 page's stylesheet

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-START npx tailwindcss -i ./styles.css -o ./dist/output.css --watch
-START npx tailwindcss -i ./nopage.css -o ./dist/nopage.css --watch
+START "Main Page" npx tailwindcss -i ./styles.css -o ./dist/output.css --watch
+START "404 Page" npx tailwindcss -i ./nopage.css -o ./dist/nopage.css --watch
