a2fd06295fc06671d4d831db94e683db806a3533
CaioMGT <florianita100.mendes@gmail.com>
2023-02-08 20:10:45 -0300

    laptob

--- run.bat
+++ run.bat
@@ -2,3 +2,4 @@ REM I don't know enough batch to convert this over to the new deploy commands lo
 START "Main Page" npx tailwindcss -i ./styles.css -o ./dist/styles.css --watch
 START "404 Page" npx tailwindcss -i ./nopage.css -o ./dist/nopage.css --watch
 START "Portfolio" npx tailwindcss -i ./portfolio.css -o ./dist/portfolio.css --watch
+START "Laptob" npx tailwindcss -i ./laptob.css -o ./dist/laptob.css --watch
