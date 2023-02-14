3d6c183ea586b2041faabafd3c38fe6d2fe9954e
CaioMGT <florianita100.mendes@gmail.com>
2023-02-14 16:26:55 -0300

    Do something for troy lol

--- run.bat
+++ run.bat
@@ -3,3 +3,4 @@ START "Main Page" npx tailwindcss -i ./styles.css -o ./dist/styles.css --watch
 START "404 Page" npx tailwindcss -i ./nopage.css -o ./dist/nopage.css --watch
 START "Portfolio" npx tailwindcss -i ./portfolio.css -o ./dist/portfolio.css --watch
 START "Laptob" npx tailwindcss -i ./laptob.css -o ./dist/laptob.css --watch
+START "Troy redirect" npx tailwindcss -i ./troy.css -o ./dist/troy.css --watch
