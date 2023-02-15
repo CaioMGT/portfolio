9d311e5195aff4739963b7576c7c4dc7de5551f5
CaioMGT <florianita100.mendes@gmail.com>
2023-02-15 18:55:25 -0300

    modular buttons

--- dist/topbar.js
+++ dist/topbar.js
@@ -1,16 +1,26 @@
 // I want this to be easily re-usable in every page (maybe other than in the 404 page?)
+function createButton(text, href) {
+    const button = document.createElement("a")
+    button.innerText = text
+    button.href = href
+    button.className = "text-right roboto text-black dark:text-white"
+    return button
+}
 class Topbar extends HTMLElement {
     constructor() {
         super()
     }
     connectedCallback() {
         // Remember: only parent elements in connectedCallback
-        const div = document.createElement("div")
-        div.className = "bg-neutral-100 dark:bg-neutral-900 w-full popup"
-        div.style = `
+        const bar = document.createElement("div")
+        bar.className = "bg-neutral-100 dark:bg-neutral-900 w-full popup"
+        bar.style = `
             height: 5vh
         `
-        this.appendChild(div)
+        const aboutme = createButton("About Me", "/")
+        bar.appendChild(aboutme)
+        // Do I need to clean up my elements on disconnectedCallback?
+        this.appendChild(bar)
     }
 }
 customElements.define("top-nav", Topbar)
