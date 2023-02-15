e9b54f02ea4fdb9e9a92ee42f086d487cf861e4f
CaioMGT <florianita100.mendes@gmail.com>
2023-02-15 18:46:08 -0300

    Working top bar element!!!!

--- dist/topbar.js
+++ dist/topbar.js
@@ -1,17 +1,15 @@
-//This will be a custom component, since I want it to be easily re-usable in every page (maybe other than in the 404 page?)
+// I want this to be easily re-usable in every page (maybe other than in the 404 page?)
 class Topbar extends HTMLElement {
     constructor() {
         super()
-        // Elements
-        const shadow = this.attachShadow({ mode: "open" })
-        const bar = document.createElement("div");
-        bar.style.width = "100%"
-        bar.style.height = "5%"
-        // Logic
-        
-        // Parenting
-        shadow.appendChild(bar) // Is this how I'm supposed to do this? I honestly have no idea
-        
+    }
+    connectedCallback() {
+        const div = document.createElement("div")
+        div.className = "bg-neutral-100 dark:bg-neutral-900 w-full popup"
+        div.style = `
+            height: 5vh
+        `
+        this.appendChild(div)
     }
 }
 customElements.define("top-nav", Topbar)
