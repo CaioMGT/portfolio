8a030bfd267d288cc04a1af052080e300fe80681
CaioMGT <florianita100.mendes@gmail.com>
2023-02-16 15:19:07 -0300

    Consolidate css files down to one

--- run.bat
+++ run.bat
@@ -1,6 +1,2 @@
 REM I don't know enough batch to convert this over to the new deploy commands lol
-START "Main Page" npx tailwindcss -i ./styles.css -o ./dist/styles.css --watch
-START "404 Page" npx tailwindcss -i ./nopage.css -o ./dist/nopage.css --watch
-START "Portfolio" npx tailwindcss -i ./portfolio.css -o ./dist/portfolio.css --watch
-START "Laptob" npx tailwindcss -i ./laptob.css -o ./dist/laptob.css --watch
-START "Troy redirect" npx tailwindcss -i ./troy.css -o ./dist/troy.css --watch
+START "Main Page" npx tailwindcss -i ./main.css -o ./dist/main.css --watch
