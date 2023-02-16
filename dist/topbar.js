3bc29a673c4bc4bcdeaf2e2e6da983ca14cf1b1d
CaioMGT <florianita100.mendes@gmail.com>
2023-02-15 21:30:21 -0300

    Some more work on the topbar

--- dist/topbar.js
+++ dist/topbar.js
@@ -3,18 +3,22 @@ function createButton(text, href) {
     const button = document.createElement("a")
     button.innerText = text
     button.href = href
-    button.className = "roboto text-black dark:text-white"
+    button.className = "roboto text-black dark:text-white top-button"
+    // Buttons shouldn't be very tall, but idk how to fix that without messing up the text. Should look how to fix this later.
     button.style = `
         padding-top: 8px;
+        border-radius: 10px;
     `
     return button
 }
+let ready = false
 class Topbar extends HTMLElement {
     static get observedAttributes() { return ['active-button']; }
     constructor() {
         super()
     }
     connectedCallback() {
+        ready = true
         // Remember: only parent elements in connectedCallback
         const bar = document.createElement("div")
         bar.className = "bg-neutral-100 dark:bg-neutral-900 w-full popup flex flex-row-reverse gap-x-6"
@@ -23,22 +27,25 @@ class Topbar extends HTMLElement {
         `
         this.buttons = {}
         const aboutme = createButton("About Me", "/")
-        aboutme.classList.add("pr-6")
+        aboutme.classList.add("mr-4")
+        aboutme.classList.add("px-2")
         bar.appendChild(aboutme)
         this.buttons.aboutme = aboutme
         const portfolio = createButton("Portfolio", "/portfolio")
         bar.appendChild(portfolio)
         this.buttons.portfolio = portfolio
-        // Do I need to clean up my elements on disconnectedCallback?
         if (this.hasAttribute("active-button")) {
             this.buttons[this.getAttribute('active-button')].classList.add("active")
         }
+        // Do I need to clean up my elements on disconnectedCallback?
         this.appendChild(bar)
     }
     attributeChangedCallback(name, oldValue, newValue) {
-        if (name == "active-button") {
-            this.buttons[newValue].classList.add("active")
-            this.buttons[oldValue].classList.remove("active")
+        if (ready) {
+            if (name == "active-button") {
+                this.buttons[newValue].classList.add("active")
+                this.buttons[oldValue].classList.remove("active")
+            }
         }
     }
 }
