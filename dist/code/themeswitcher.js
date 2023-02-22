1ab5cb65c6f0a927d9186cb0a50a167bb3bbd605
CaioMGT <florianita100.mendes@gmail.com>
2023-02-21 23:25:37 -0300

    WIP toggle button

--- dist/code/themeswitcher.js
+++ dist/code/themeswitcher.js
@@ -1,13 +1,20 @@
 window.addEventListener("load", () => {
     let button = document.getElementById('theme-switcher')
+    if (theme == Enum.LIGHT) {
+
+        button.classList.remove("normal")
+        button.classList.add("active")
+    }
     button.addEventListener("click", () => {
-        if (theme == Enum.LIGHT) {
+        if (button.classList.contains("active")) {
             console.log("Switching theme to dark mode")
-            theme = Enum.DARK
+            button.classList.remove("active")
+            button.classList.add("normal")
             changetheme(Enum.DARK, true)
         } else {
             console.log("Switching theme to light mode")
-            theme = Enum.LIGHT
+            button.classList.remove("normal")
+            button.classList.add("active")
             changetheme(Enum.LIGHT, true)
         }
     })
