7b94d08fa5e1ff33d3030f8cf3251f896faa0c18
CaioMGT <florianita100.mendes@gmail.com>
2023-02-21 23:03:02 -0300

    Move theme switcher to settings

--- dist/code/themeswitcher.js
+++ dist/code/themeswitcher.js
@@ -1,21 +1,14 @@
 window.addEventListener("load", () => {
     let button = document.getElementById('theme-switcher')
-    if (theme == Enum.DARK) {
-        button.innerText = "Enable Light Mode"
-    } else {
-        button.innerText = "Enable Dark Mode"
-    }
     button.addEventListener("click", () => {
         if (theme == Enum.LIGHT) {
             console.log("Switching theme to dark mode")
             theme = Enum.DARK
             changetheme(Enum.DARK, true)
-            button.innerText = "Enable Light Mode"
         } else {
             console.log("Switching theme to light mode")
             theme = Enum.LIGHT
             changetheme(Enum.LIGHT, true)
-            button.innerText = "Enable Dark Mode"
         }
     })
 })
