2839b6cdd6e1b804ea8e5cfa139e5cb1b62ea3d6
CaioMGT <florianita100.mendes@gmail.com>
2023-02-23 15:55:24 -0300

    Add bottom bar

--- dist/code/bars.js
+++ dist/code/bars.js
@@ -6,9 +6,7 @@ function createButton(text, href) {
     button.href = href
     button.className = "roboto text-black dark:text-white top-button px-2 py-2"
     // Buttons shouldn't be very tall, but idk how to fix that without messing up the text. Should look how to fix this later.
-    button.style = `
-        border-radius: 15px;
-    `
+    button.style.borderRadius = "15px"
     button.addEventListener("click", () => {
         if (currentButton) {
             currentButton.classList.remove("activeTop")
@@ -37,7 +35,7 @@ class Topbar extends HTMLElement {
         ready = true
         // Remember: only parent elements in connectedCallback
         const bar = document.createElement("div")
-        bar.className = "bg-neutral-100 dark:bg-neutral-900 w-full popup flex flex-row-reverse gap-x-6"
+        bar.className = "bg-neutral-100 dark:bg-neutral-900 w-full popup flex flex-row-reverse gap-x-6 roboto"
         bar.style = `
             height: 40px;
             position: -webkit-sticky; /* safari */
@@ -78,3 +76,38 @@ class Topbar extends HTMLElement {
     }
 }
 customElements.define("top-nav", Topbar)
+class BottomBar extends HTMLElement {
+    static get observedAttributes() { return ['bottom']}
+    constructor() {
+        super()
+    }
+    connectedCallback() {
+        const bar = document.createElement("div")
+        bar.className = "bg-neutral-100 dark:bg-neutral-900 w-full flex flex-row-reverse gap-x-6 roboto text-black dark:text-white mt-10"
+        bar.style.height = "100px;"
+        if (this.hasAttribute("bottom")) {
+            bar.style.position = "absolute" 
+            bar.style.bottom = "0px"
+        }
+        const contact = document.createElement("div")
+        contact.className = "flex flex-col h-auto roboto content-center items-center"
+        contact.innerText = "Contact:"
+        const discordContainer = document.createElement("div")
+        const discordLogo = document.createElement("img")
+        discordLogo.src = "./svg/discord.svg"
+        discordLogo.style.display = "inline"
+        discordLogo.style.height = "25px"
+        discordLogo.style.width = "25px"
+        const discord = document.createElement("h1")
+        discord.innerText = "capetaanal#1984"
+        discord.style.display = "inline"
+        discord.classList.add("ml-1")
+        discord.classList.add("mr-2")
+        discordContainer.appendChild(discordLogo)
+        discordContainer.appendChild(discord)
+        contact.appendChild(discordContainer)
+        bar.appendChild(contact)
+        this.appendChild(bar)
+    }
+}
+customElements.define("bottom-bar", BottomBar)
