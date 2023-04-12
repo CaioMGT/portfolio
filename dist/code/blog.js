5a994d171e0e0ab568d60e47a6a7546d81c30dfe
CaioMGT <caio@caiomgt.com>
2023-04-11 22:35:46 -0300

    stuff

--- dist/code/blog.js
+++ dist/code/blog.js
@@ -1,10 +1,9 @@
-let box = document.getElementById("postBox")
 const getPosts = new XMLHttpRequest()
 let postList
 function createPostPreview(post) {
     const bg = document.createElement("div")
     const title = document.createElement("h2")
-    title.className = ""
+    title.className = "title"
     title.innerText = post.title
     bg.appendChild(title)
     return bg
@@ -14,11 +13,13 @@ getPosts.send(JSON.stringify({"type": "list"}))
 getPosts.onload = function() {
     postList = JSON.parse(getPosts.responseText).posts
     console.log(postList)
+    let box = document.getElementById("postBox")
     for (post of postList) {
-        if (post.id) {
+        if (post.id != null) {
             // for some reason the response is returning a random prototype
             // along with the posts so i have to do this.
             const postThing = createPostPreview(post)
+            console.log(postThing)
             box.appendChild(postThing)
         }
     }
--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 REM I don't know enough batch to convert this over to the new deploy commands lol
 START "Main Page" npx tailwindcss -i ./main.css -o ./dist/main.css --watch
+START "Main Page" npx tailwindcss -i ./blog.css -o ./dist/blog.css --watch
