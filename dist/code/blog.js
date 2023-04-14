c459df482c5c2538f580b2c2c29ccf441a0cc814
CaioMGT <caio@caiomgt.com>
2023-04-14 19:41:09 -0300

    i will murder someone

--- dist/code/blog.js
+++ dist/code/blog.js
@@ -1,18 +1,7 @@
-function fetchThing(url, code) {
-    fetch(url).then((res) => res.json()).then(code)
-}
-function createPostPreview(post) {
-    const bg = document.createElement("div")
-    const title = document.createElement("h2")
-    title.className = "title"
-    title.innerText = post.title
-    bg.appendChild(title)
-    return bg
-}
-fetchThing("https://api.caiomgt.com/getPosts", function(json) {
-    const postList = json.post
+async function summonPosts() {
+    const postList= (await (await fetch("https://api.caiomgt.com/getPosts")).json()).post;
     console.log(postList)
-    let box = document.getElementById("postBox")
+    const box = document.getElementById("postBox")
     for (post of postList) {
         if (post.id != null) {
             // for some reason the response is returning a random prototype
@@ -22,5 +11,29 @@ fetchThing("https://api.caiomgt.com/getPosts", function(json) {
             box.appendChild(postThing)
         }
     }
-})
+}
+async function checkIfAdmin(password) {
+    const response = await fetch("https://api.caiomgt.com/validatePassword", {method: "POST", headers:{"Content-Type": "application/json"}, body:JSON.stringify({auth: password})})
+    console.log(response.body)
+    const json = await response.json()
+    return json.auth 
+}
+function createPostPreview(post) {
+    const bg = document.createElement("div")
+    const title = document.createElement("h2")
+    title.className = "title"
+    title.innerText = post.title
+    bg.appendChild(title)
+    return bg
+}
+summonPosts()
 
+if (localStorage.getItem("password")) {
+    checkIfAdmin(localStorage.getItem('password')).then((thing) => {
+        if (thing){
+            console.log("your mom")
+        } else {
+            console.log("not your mom")
+        }
+    })
+}
