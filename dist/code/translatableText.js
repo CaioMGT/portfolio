a4a9dfdd4dacdfd465101673963c96a3c104b169
CaioMGT <caio@caiomgt.com>
2023-05-04 17:47:46 -0300

    dynamic language switching funcionality poggies

--- dist/code/translatableText.js
+++ dist/code/translatableText.js
@@ -1,19 +1,42 @@
 const elements = [];
 let lang = localStorage.getItem("lang") || "en";
-let en = await (await fetch("en-us.json")).json();
-let pt = await (await fetch("pt-br.json")).json();
+let en;
+let pt;
+fetch("en-us.json").then(function (val) {
+  val.json().then(function (json) {
+    en = json;
+  });
+});
+fetch("pt-br.json").then(function (val) {
+  val.json().then(function (json) {
+    pt = json;
+  });
+});
+function waitFor(conditionFunction) {
+  const poll = (resolve) => {
+    if (conditionFunction()) resolve();
+    else setTimeout((_) => poll(resolve), 400);
+  };
 
-function update(transText) {
+  return new Promise(poll);
+}
+async function update(transText) {
   let string;
+  if (en == undefined || pt == undefined) {
+    console.log("waiting for lang files to load");
+    await waitFor(() => !(en == undefined) && !(pt == undefined));
+    update(transText);
+    return;
+  }
   if (lang == "en") {
     string = en[transText.id];
-  } else {
+  } else if (lang == "pt") {
     string = pt[transText.id];
   }
   console.log("string is " + string);
   transText.innerText = string;
 }
-export function changeLang(Lang) {
+function changeLang(Lang) {
   lang = Lang;
   console.log(elements);
   elements.forEach(function (element) {
@@ -25,8 +48,8 @@ class transText extends HTMLElement {
   constructor() {
     super();
   }
-  connectedCallback() {
-    elements[this.id] = this;
+  async connectedCallback() {
+    elements.push(this);
     console.log("added to elements " + elements[this.id]);
     update(this);
   }
