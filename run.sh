d14c967d085be9dc71f996b61d7049653b0ecb7f
CaioMGT <caio@caiomgt.com>
2023-06-08 12:20:44 -0300

    Add disqus powered comments

--- run.sh
+++ run.sh
@@ -3,3 +3,5 @@ npx tailwindcss -i ./main.css -o ./dist/main.css --watch &
 disown
 npx tailwindcss -i ./blog.css -o ./dist/blog.css --watch &
 disown
+npx tailwindcss -i ./post.css -o ./dist/post.css --watch &
+disown
