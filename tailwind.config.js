ae55afe2e1eef014daa2b4cefdeae1671669f73c
CaioMGT <caio@caiomgt.com>
2023-07-04 19:16:22 -0300

    stop hardcoding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "index.html"],
   theme: {
     extend: {},
-    colors: {
-      black: "#000000",
-      orange: "#7C3612",
-      bg: "#171717",
-      white: "#FFFFFF",
-      lightorange: "#FF7709",
-    },
   },
   plugins: [],
 };
