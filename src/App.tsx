7b06b3aaab60a2723aed1216d94af6b2179f8a1f
CaioMGT <caio@caiomgt.com>
2023-07-04 21:04:28 -0300

    Use solid router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { Show, type Component, createSignal } from "solid-js";
+function setTitle(title: string) {
+  document.title = title;
+}
+setTitle("Main Page");
 const App: Component = () => {
   return <div>Hello World!</div>;
 };
