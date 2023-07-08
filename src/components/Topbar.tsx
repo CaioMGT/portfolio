c8ea08940e3c9da246cdbdf3f8e5125a9ce6d278
CaioMGT <caio@caiomgt.com>
2023-07-08 18:10:30 -0300

    styled topbar button + more content + new favicon

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,7 +1,11 @@
 import { A } from "@solidjs/router";
 
 function TopbarButton(props: { name: string; href: string }) {
-  return <A href={props.href}>{props.name}</A>;
+  return (
+    <A href={props.href} class="hover:bg-slate-300/30 rounded-lg px-2 py-2">
+      {props.name}
+    </A>
+  );
 }
 export default function Topbar(props: { activeButton: string }) {
   return (
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,8 +12,12 @@ export default function () {
   return (
     <div>
       <Topbar activeButton=""></Topbar>
-      <div class="md:grid grid-cols-2 mx-4 px-8 h-full justify-between">
-        <div class="my-12 text-slate-300">
+      <div class="md:grid grid-cols-2 mx-4 px-8 h-full justify-center overflow-x-hidden">
+        <div class="my-12 text-slate-300 lg:pr-32 xl:pr-64">
+          {/* I have to animate transform because the motion one wiki says using x: is not hardware accelerated,
+              which is a no-no for performance. 
+              Check https://motion.dev/guides/performance#css-variables-and-individual-transforms for more info
+              */}
           <Motion
             class="text-6xl text-white"
             animate={{
@@ -42,9 +46,32 @@ export default function () {
               front-end and back-end applications. Starting at an early age,
               I've started developing applications at the age of 10, with the
               goal of becoming a professional programmer. I'm currently still on
-              that path and plan to major in Computer Science. I'm trans and use
-              She/Her pronouns.
+              that path and plan to major in Computer Science. I'm trans-fem and
+              use She/Her pronouns.
             </div>
+          </Motion>
+        </div>
+        <div class="my-12 text-slate-200  lg:pl-32 xl:pl-64">
+          <Motion
+            class="text-6xl text-white pb-2"
+            animate={{
+              transform: ["translateX(1000px)", null, "translateX(0px)"],
+            }}
+            transition={{ duration: 4, transform: { offset: [0, 0.8, 1] } }}
+          >
+            My <span class="glow text-pink-200">Likes </span>
+          </Motion>
+          <Motion
+            animate={{ opacity: [0, null, 1] }}
+            transition={{ duration: 6, opacity: { offset: [0, 0.9, 1] } }}
+          >
+            I like playing video games, developing them and generally anything
+            tech-related. I've experimented with a little bit of hardware
+            engineering using my arduino and breadboards before but decided it's
+            not the right field for me. I have a fascination with modding games
+            and would love to some day learn how to make my own mods for the
+            games I play. My favorite programming languages are Typescript, Lua
+            and C#.
           </Motion>
         </div>
       </div>
