c0790501092713ddebeb0dc3728342f616440af1
CaioMGT <caio@caiomgt.com>
2023-07-08 10:31:57 -0300

    movement woah

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 document.title = "Home";
 import Topbar from "../components/Topbar";
+import { Motion } from "@motionone/solid";
 const date = new Date();
 const birthday = new Date("April 10 2008");
 let age = date.getFullYear() - birthday.getFullYear();
@@ -13,19 +14,34 @@ export default function () {
       <Topbar activeButton=""></Topbar>
       <div class="md:grid grid-cols-2 mx-4 px-8 h-full justify-between">
         <div class="my-12 text-slate-300">
-          <div class="text-6xl text-white">
+          <Motion
+            class="text-6xl text-white"
+            animate={{ x: [-500, -500, 0] }}
+            transition={{ duration: 1, x: { offset: [0, 0.5, 1] } }}
+          >
             Hi, I'm <span class="glow text-pink-200">Ellie</span>
-          </div>
-          <div class="text-2xl">Programmer, UI Designer</div>
-          <div class="text-1xl">Here's a little about me:</div>
-          <div>
-            I'm a {age} year old from Brazil, well-versed in programming both
-            front-end and back-end applications. Starting at an early age, I've
-            started developing applications at the age of 10, with the goal of
-            becoming a professional programmer. I'm currently still on that path
-            and plan to major in Computer Science. I'm trans and use She/Her
-            pronouns.
-          </div>
+          </Motion>
+          <Motion
+            class="text-2xl"
+            animate={{ x: [-500, -500, 0] }}
+            transition={{ duration: 2, x: { offset: [0, 0.5, 1] } }}
+          >
+            Programmer, UI Designer
+          </Motion>
+          <Motion
+            animate={{ opacity: [0, 0, 1] }}
+            transition={{ duration: 4, opacity: { offset: [0, 0.75, 1] } }}
+          >
+            <div class="text-1xl">Here's a little about me:</div>
+            <div>
+              I'm a {age} year old from Brazil, well-versed in programming both
+              front-end and back-end applications. Starting at an early age,
+              I've started developing applications at the age of 10, with the
+              goal of becoming a professional programmer. I'm currently still on
+              that path and plan to major in Computer Science. I'm trans and use
+              She/Her pronouns.
+            </div>
+          </Motion>
         </div>
       </div>
     </div>
